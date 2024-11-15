de4e5d30a5ef9417ab48b1703e048538f9aa7ea6
Katalin Fazekas <k.katalin.fazekas@gmail.com>
2024-11-15 17:23:49 +0100

    mobical side of propagate

--- src/mobical.cpp
+++ src/mobical.cpp
@@ -1204,41 +1204,42 @@ struct Call {
     SIMPLIFY        = shift ( 12 ),
     LOOKAHEAD       = shift ( 13 ),
     CUBING          = shift ( 14 ),
+    PROPAGATE       = shift ( 15 ),
 
-    VAL             = shift ( 15 ),
-    FLIP            = shift ( 16 ),
-    FLIPPABLE       = shift ( 17 ),
-    FAILED          = shift ( 18 ),
-    FIXED           = shift ( 19 ),
+    VAL             = shift ( 16 ),
+    FLIP            = shift ( 17 ),
+    FLIPPABLE       = shift ( 18 ),
+    FAILED          = shift ( 19 ),
+    FIXED           = shift ( 20 ),
 
-    FREEZE          = shift ( 20 ),
-    FROZEN          = shift ( 21 ),
-    MELT            = shift ( 22 ),
+    FREEZE          = shift ( 21 ),
+    FROZEN          = shift ( 22 ),
+    MELT            = shift ( 23 ),
 
-    LIMIT           = shift ( 23 ),
-    OPTIMIZE        = shift ( 24 ),
+    LIMIT           = shift ( 24 ),
+    OPTIMIZE        = shift ( 25 ),
 
-    DUMP            = shift ( 25 ),
-    STATS           = shift ( 26 ),
+    DUMP            = shift ( 26 ),
+    STATS           = shift ( 27 ),
 
-    RESET           = shift ( 27 ),
+    RESET           = shift ( 28 ),
 
-    CONSTRAIN       = shift ( 28 ),
+    CONSTRAIN       = shift ( 29 ),
 
-    CONNECT         = shift ( 29 ),
-    OBSERVE         = shift ( 30 ),
-    LEMMA           = shift ( 31 ),
+    CONNECT         = shift ( 30 ),
+    OBSERVE         = shift ( 31 ),
+    LEMMA           = shift ( 32 ),
 
-    CONCLUDE        = shift ( 32 ),
-    DISCONNECT      = shift ( 33 ),
+    CONCLUDE        = shift ( 33 ),
+    DISCONNECT      = shift ( 34 ),
 
-    TRACEPROOF      = shift ( 34 ),
-    FLUSHPROOFTRACE = shift ( 35 ),
-    CLOSEPROOFTRACE = shift ( 36 ),
+    TRACEPROOF      = shift ( 35 ),
+    FLUSHPROOFTRACE = shift ( 36 ),
+    CLOSEPROOFTRACE = shift ( 37 ),
 
 #ifdef MOBICAL_MEMORY
-    MAXALLOC        = shift ( 37 ),
-    LEAKALLOC       = shift ( 38 ),
+    MAXALLOC        = shift ( 38 ),
+    LEAKALLOC       = shift ( 39 ),
 #endif
 
     // clang-format on
@@ -1253,7 +1254,7 @@ struct Call {
     CONFIG = INIT | SET | CONFIGURE | ALWAYS | TRACEPROOF,
     BEFORE = ADD | CONSTRAIN | ASSUME | ALWAYS | DISCONNECT | CONNECT |
              OBSERVE,
-    PROCESS = SOLVE | SIMPLIFY | LOOKAHEAD | CUBING,
+    PROCESS = SOLVE | SIMPLIFY | LOOKAHEAD | CUBING | PROPAGATE,
     DURING = LEMMA, // | CONTINUE,
     AFTER = VAL | FLIP | FLIPPABLE | FAILED | CONCLUDE | ALWAYS |
             FLUSHPROOFTRACE | CLOSEPROOFTRACE,
@@ -1561,6 +1562,17 @@ struct CubingCall : public Call {
   void print (ostream &o) { o << "cubing " << res << endl; }
   Call *copy () { return new CubingCall (res); }
   const char *keyword () { return "cubing"; }
+};
+
+struct PropagateCall : public Call {
+  PropagateCall (int r = 0) : Call (PROPAGATE, 0, r) {}
+  void execute (Solver *&s) { 
+    std::vector<int> implicants; 
+    (void) s->propagate (implicants);
+  }
+  void print (ostream &o) { o << "propagate " << res << endl; }
+  Call *copy () { return new PropagateCall (res); }
+  const char *keyword () { return "propagate"; }
 };
 
 struct ValCall : public Call {
@@ -1891,8 +1903,6 @@ public:
             Call *next_c = calls[j];
             if (next_c->type == Call::LEMMA)
               next_c->execute (solver);
-            // else if (next_c->type == Call::CONTINUE)
-            //   next_c->execute (solver);
             else
               break;
           }
@@ -2658,9 +2668,11 @@ void Trace::generate_process (Random &random) {
   } else if (fraction > 0.99) {
     const int depth = random.pick_int (0, 10);
     push_back (new CubingCall (depth));
-  } else if (fraction > 0.9)
+  } else if (fraction > 0.9) {
     push_back (new LookaheadCall ());
-  else {
+  } else if (fraction > 0.85) {
+    push_back (new PropagateCall ());
+  } else {
     const int rounds = random.pick_int (0, 10);
     push_back (new SimplifyCall (rounds));
   }
@@ -3477,6 +3489,7 @@ static bool is_basic (Call *c) {
   case Call::SIMPLIFY:
   case Call::LOOKAHEAD:
   case Call::CUBING:
+  case Call::PROPAGATE:
   case Call::VARS:
   case Call::ACTIVE:
   case Call::REDUNDANT:
@@ -4146,8 +4159,6 @@ void Reader::parse () {
       // if (!lemma_adding && !lit) error ("empty lemma is learned.");
       lemma_adding = lit;
       c = new LemmaCall (lit);
-      // } else if (!strcmp (keyword, "continue")) {
-      //   c = new ContinueCall ();
     } else if (!strcmp (keyword, "assume")) {
       if (!first)
         error ("argument to 'assume' missing");
@@ -4200,6 +4211,16 @@ void Reader::parse () {
       assert (!second);
       c = new CubingCall (lit);
       solved++;
+    } else if (!strcmp (keyword, "propagate")) {
+      if (first && !parse_int_str (first, lit))
+        error ("invalid argument '%s' to 'solve'", first);
+      if (first && lit != 0 && lit != 10 && lit != 20)
+        error ("invalid result argument '%d' to 'solve'", lit);
+      assert (!second);
+      if (first)
+        c = new PropagateCall (lit);
+      else
+        c = new PropagateCall ();
     } else if (!strcmp (keyword, "val")) {
       if (!first)
         error ("first argument to 'val' missing");
@@ -4427,9 +4448,8 @@ void Reader::parse () {
         }
         assert (state == Call::SOLVE || state == Call::SIMPLIFY ||
                 state == Call::LOOKAHEAD || state == Call::CUBING ||
-                state == Call::OBSERVE || state == Call::LEMMA ||
-                // state == Call::CONTINUE ||
-                state == Call::AFTER);
+                state == Call::PROPAGATE || state == Call::OBSERVE ||
+                state == Call::LEMMA || state == Call::AFTER);
         new_state = Call::AFTER;
         break;
 
@@ -4437,6 +4457,7 @@ void Reader::parse () {
       case Call::SIMPLIFY:
       case Call::LOOKAHEAD:
       case Call::CUBING:
+      case Call::PROPAGATE:
       case Call::RESET:
       case Call::CONNECT:
       case Call::LEMMA:
